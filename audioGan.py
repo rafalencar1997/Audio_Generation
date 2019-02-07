7490019041ea210d892a3620473ecbef398a095e
Rafael Alencar <rafalencar@rafael-mbp.lan>
2019-02-07 17:36:30 -0200

    Best Results

--- audioGan.py
+++ audioGan.py
@@ -13,70 +13,84 @@ from ganSetup import *
 class AudioGAN:
     def __init__(self, label="Saxophone"):
 
-        # Generate Models
-        self.G = generator(NOISE_DIM, AUDIO_SHAPE)
-        self.D = discriminator(AUDIO_SHAPE)
-        self.stacked_G_D = stacked_G_D(self.G, self.D)
+        # Generate models
+        self.enc = encoder(AUDIO_SHAPE, ENCODE_SIZE)
+        self.gen = generator(NOISE_DIM, AUDIO_SHAPE)
+        self.dis = discriminator(AUDIO_SHAPE)
+        self.stackGenDis = stacked_G_D(self.gen, self.dis)
+        self.autoencoder = autoEncoder(self.enc, self.gen)  
+        
+        # Compile models
+        self.opt = Adam(lr = 0.0002, beta_1 = 0.5)
+        self.gen.compile(loss = 'binary_crossentropy', optimizer = self.opt, metrics = ['accuracy'])
+        self.dis.compile(loss = 'binary_crossentropy', optimizer = self.opt, metrics = ['accuracy'])
+        self.autoencoder.compile(loss = 'mse',         optimizer = self.opt, metrics = ['accuracy'])
+        self.stackGenDis.compile(loss = 'binary_crossentropy', optimizer = self.opt,  metrics = ['accuracy'])
 
-        # Compile Models
-        self.Optimizer = Adam(lr = 0.0002, beta_1 = 0.5)
-        self.G.compile(loss='binary_crossentropy', optimizer=self.Optimizer, metrics=['accuracy'])
-        self.D.compile(loss='binary_crossentropy', optimizer=self.Optimizer, metrics=['accuracy'])
-        self.stacked_G_D.compile(loss='binary_crossentropy', optimizer=self.Optimizer,  metrics=['accuracy'])
+        # Set training data
+        self.trainData = normalization(load_train_data(AUDIO_SHAPE, label))
 
-        self.X_train = normalization(load_train_data(AUDIO_SHAPE, label))
-
-        self.D_loss_hist = []
-        self.G_loss_hist = []
+        self.disLossHist = []
+        self.genLossHist = []
 
     # Train GAN
-    def train(self, epochs=20, batch=32, save_interval=2):
+    def train_gan(self, epochs = 20, batch = 32, save_interval = 2):
         for cnt in range(epochs):
           
             # Train discriminator
             halfBatch = int(batch/2)
-            random_index = np.random.randint(0, len(self.X_train) - halfBatch)
-            legit_audios = self.X_train[random_index: int(random_index + halfBatch)]
+            random_index = np.random.randint(0, len(self.trainData) - halfBatch)
+            legit_audios = self.trainData[random_index: int(random_index + halfBatch)]
             gen_noise = np.random.normal(0, 1, (halfBatch, NOISE_DIM))
-            syntetic_audios = self.G.predict(gen_noise)
+            syntetic_audios = self.gen.predict(gen_noise)
             x_combined_batch = np.concatenate((legit_audios, syntetic_audios))
             y_combined_batch = np.concatenate((np.ones((halfBatch, 1)), np.zeros((halfBatch, 1))))
-
-            # Update stacked discriminator Weights
-            d_loss = self.D.train_on_batch(x_combined_batch, y_combined_batch)
-
-            self.stacked_G_D.layers[1].set_weights(self.D.get_weights())
-            stacked_D_Weights = self.stacked_G_D.layers[1].get_weights()
-            D_Weights = self.D.get_weights()
-
+            d_loss = self.dis.train_on_batch(x_combined_batch, y_combined_batch)
+            
+            # Update stacked discriminator weights
+            self.stackGenDis.layers[1].set_weights(self.dis.get_weights())
+    
+            # Include discriminator loss
             d_loss_mean = np.mean(d_loss)
-            self.D_loss_hist.append(d_loss_mean)
+            self.disLossHist.append(d_loss_mean)
 
             # Train stacked generator
             noise = np.random.normal(0, 1, (batch, NOISE_DIM))
             y_mislabled = np.ones((batch, 1))
-            g_loss = self.stacked_G_D.train_on_batch(noise, y_mislabled)
+            g_loss = self.stackGenDis.train_on_batch(noise, y_mislabled)
 
             # Update generator Weights
-            self.G.set_weights(self.stacked_G_D.layers[0].get_weights())
-
+            self.gen.set_weights(self.stackGenDis.layers[0].get_weights())
+            
+            # Include generator loss
             g_loss_mean = np.mean(g_loss)
-            self.G_loss_hist.append(g_loss_mean)
+            self.genLossHist.append(g_loss_mean)
+            
             if cnt % int(save_interval/2) == 0:
                 print("epoch: %d" % (cnt))
                 print("Discriminator_loss: %f, Generator_loss: %f" % (d_loss_mean, g_loss_mean))
             if cnt % save_interval == 0:
-                self.show_samples(4)
-            
-    def show_samples(self, samples = 4):
-        fig     = plt.figure(figsize=(1, samples))
+                self.show_gen_samples(4)
+     
+    # Plot a number of generated samples
+    def show_gen_samples(self, samples = 4):
         samplePlot = []
-        noise = np.random.normal(0, 1, (samples,NOISE_DIM))
-        audios = self.G.predict(noise)
+        fig        = plt.figure(figsize = (1, samples))
+        noise      = np.random.normal(0, 1, (samples,NOISE_DIM))
+        audios     = self.gen.predict(noise)        
         for i, audio in enumerate(audios):
-            IPython.display.display(ipd.Audio(data=audio, rate=SAMPLE_RATE))
+            IPython.display.display(ipd.Audio(data = audio, rate = SAMPLE_RATE))
             samplePlot.append(fig.add_subplot(1, samples, i+1))
             samplePlot[i].plot(audio.flatten(), '.', )
         plt.gcf().set_size_inches(25, 5)
         plt.subplots_adjust(wspace=0.3,hspace=0.3)
         plt.show()
+       
+    # Train autoencoder
+    def train_autoencoder(self, epochs = 20,  save_internal = 2, batch = 32):
+        for cnt in range(epochs):
+            random_index = np.random.randint(0, len(self.trainData) - batch)
+            legit_audios = self.trainData[random_index : int(random_index + batch)]
+            loss = self.autoencoder.train_on_batch(legit_audios, legit_audios)
+            if cnt % save_internal == 0 : 
+                print("Epoch: ", cnt, ", Loss: ", loss)
--- ganModels.py
+++ ganModels.py
@@ -5,47 +5,48 @@ from keras import losses, models, optimizers
 from keras.optimizers import Adam
 from keras.models import Sequential
 from keras.layers import (Layer, Input, Flatten, Dropout, BatchNormalization, Reshape,
-                          MaxPool1D, AveragePooling1D, GlobalAveragePooling1D, GlobalAveragePooling2D,
+                          MaxPool1D, AveragePooling1D, AveragePooling2D, GlobalAveragePooling1D, GlobalAveragePooling2D,
                           Conv2DTranspose, Conv1D, Dense, LeakyReLU, ReLU, Activation,
                           LSTM, SimpleRNNCell)
 
 # Generator Model
 def generator(NoiseDim, OutputShape):
     model = Sequential()
-    model.add(Dense(2100, input_shape=(NoiseDim,)))
+    model.add(Dense(1000, input_shape=(NoiseDim,)))
     model.add(LeakyReLU(alpha=0.01))
     model.add(BatchNormalization(momentum=0.9))
-    model.add(Reshape((2100, 1)))
+    model.add(Reshape((1000, 1)))
 
-    model.add(Conv1D(32, 7, padding='same'))
-    model.add(Activation('linear'))
+    model.add(Conv1D(16, 50, padding='same'))
+    model.add(ReLU())
     model.add(BatchNormalization(momentum=0.9))
     model.add(Dropout(rate=0.1))
 
-    model.add(Conv1D(64, 5, padding='same'))
-    model.add(Activation('linear'))
+    model.add(Conv1D(32, 25, padding='same'))
+    model.add(ReLU())
     model.add(BatchNormalization(momentum=0.9))
     model.add(Dropout(rate=0.1))
 
-    model.add(Conv1D(128, 3, padding='same'))
-    model.add(Activation('linear'))
+    model.add(Conv1D(64, 15, padding='same'))
+    model.add(ReLU())
     model.add(BatchNormalization(momentum=0.9))
     model.add(Dropout(rate=0.1))
  
-    model.add(Conv1D(420, 11, padding='same'))
+    model.add(Conv1D(64, 11, padding='same'))
     model.add(Activation('linear'))
-    model.add(AveragePooling1D(20))
-    model.add(BatchNormalization(momentum=0.9))
-    model.add(Dropout(rate=0.1))
+    #model.add(BatchNormalization(momentum=0.9))
+    model.add(Dropout(rate=0.3))
     model.add(Flatten())
     return model
 
 # Discriminator Model
 def discriminator(InputShape):
     model = Sequential()
+    
     model.add(Reshape((InputShape, 1), input_shape=(InputShape,)))
     model.add(Conv1D(32, 11, strides=7, padding='valid'))
     model.add(Activation('linear'))
+    model.add(AveragePooling1D(4))
     model.add(BatchNormalization(momentum=0.9))
     model.add(Dropout(rate=0.1))
 
@@ -63,7 +64,7 @@ def discriminator(InputShape):
     model.add(Dense(1024))
     model.add(LeakyReLU(alpha=0.01))
     model.add(BatchNormalization(momentum=0.9))
-    model.add(Dense(1, activation='tanh'))
+    model.add(Dense(1, activation='sigmoid'))
     return model
 
 # Stacked Generator and Discriminator
@@ -101,44 +102,4 @@ def autoEncoder(Encoder, Generator):
     model = Sequential()
     model.add(Encoder)
     model.add(Generator)
-    return model
-
-def discriminator2():
-    model = Sequential()
-    model.add(Reshape((SHAPE, 1), input_shape=(SHAPE,)))
-    model.add(Conv1D(32, 5, strides=4, padding='valid'))
-    model.add(LeakyReLU(alpha=0.01))
-    model.add(MaxPool1D(pool_size=4, strides=4, padding='valid'))
-    
-    model.add(Conv1D(64, 5, strides=34, padding='valid'))
-    model.add(LeakyReLU(alpha=0.01))
-    model.add(MaxPool1D(pool_size=4, strides=4, padding='valid'))
-    
-    model.add(Flatten())
-    model.add(Dense(NOISE_DIM))
-    model.add(LeakyReLU(alpha=0.01))
-    model.add(Dense(1))
-    model.add(LeakyReLU(alpha=0.01))
-    return model
-
-def generator2():
-    model = Sequential()
-    model.add(Dense(DENSE_SIZE, input_shape=(NOISE_DIM,)))
-    model.add(Activation('linear'))
-    #model.add(ReLU())
-    model.add(BatchNormalization(momentum=0.9))
-    
-    model.add(Reshape((DENSE_SIZE,1,1)))
-    
-    model.add(Conv2DTranspose(64, kernel_size=(4,1), strides=(3, 1), padding='same'))
-    model.add(Activation('linear'))
-    #model.add(ReLU())
-    model.add(BatchNormalization(momentum=0.9))
-    
-    model.add(Conv2DTranspose(1, kernel_size=(4,1), strides=(7, 1), padding='same'))
-    model.add(Activation('linear'))
-    
-    #model.add(GlobalAveragePooling2D())
-    
-    model.add(Flatten())
     return model
